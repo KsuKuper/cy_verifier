8a626a87ca8541f2d974abe1327b3d45182bcf55
KsuKuper <nekrashevich.k@mail.ru>
2024-03-04 08:21:29 +0300

    updated hw_5.2.2

--- cypress/e2e/1-getting-started/verifier_header.cy.js
+++ cypress/e2e/1-getting-started/verifier_header.cy.js
@@ -5,14 +5,40 @@ describe("Verifier App", () => {
   });
   it("Navbar are displayed", () => {
     cy.get("[data-cy='navbar']").should("be.visible");
+  });
+  it("Navbar are displayed the label", () => {
     cy.get("img").should("be.visible");
+  });
+  it("Navbar are displayed the brend", () => {
     cy.get(".brand-title > span").should("be.visible");
+  });
+  it("Navbar are displayed the vertion", () => {
     cy.get(".navbar-version").should("be.visible");
+  });
+  it("Navbar are displayed the home", () => {
     cy.get(":nth-child(1) > .d-flex").should("be.visible");
-    cy.get(":nth-child(1) > .d-flex").click();
+  });
+  it("Navbar are displayed the language", () => {
     cy.get(":nth-child(2) > .d-flex").should("be.visible");
-    cy.get(":nth-child(2) > .d-flex").click();
+    cy.get(":nth-child(2) > .d-flex").click(); // selector #header-tabs > li:nth-child(2) > a - href="#" (заглушка)
+    cy.get("[data-cy='accountMenu'] > .d-flex").should("have.length", 4); //ER = 4, AR = 1
+  });
+  it("Navbar are displayed the account", () => {
     cy.get("[data-cy='accountMenu'] > .d-flex").should("be.visible");
-    cy.get("[data-cy='accountMenu'] > .d-flex").click();
+    cy.get("[data-cy='accountMenu'] > .d-flex").click(); // href="#" (заглушка)
+    cy.get("[data-cy='accountMenu'] > .d-flex").should("have.length", 2); //ER = 2, AR = 1
+    //sign in
+    cy.get("[data-cy='login']").find("[a#login-item.dropdown-item]").click();
+    cy.url("https://sqlverifier-live-6e21ca0ed768.herokuapp.com/login").should(
+      "include",
+      "/login"
+    ); // ER = /login, AR = /account/register
+    //register
+    cy.get("[data-cy='register']")
+      .find("#account-menu > div > a:nth-child(2)")
+      .click();
+    cy.url(
+      "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/account/register"
+    ).should("include", "/account/register");
   });
 });
