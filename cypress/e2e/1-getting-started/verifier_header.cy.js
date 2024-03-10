9120afac5aaf7be188b7c0fb682cc4d5484bdccd
KsuKuper <nekrashevich.k@mail.ru>
2024-03-10 18:10:33 +0300

    updated_hw_5.2.2, another file has been added with a test of navigation through the header menu of the verifier app

--- cypress/e2e/1-getting-started/verifier_header.cy.js
+++ cypress/e2e/1-getting-started/verifier_header.cy.js
@@ -1,7 +1,7 @@
 describe("Verifier App", () => {
   beforeEach("Open Verifier app", () => {
     cy.visit("/?page=1&sort=id,asc");
-    // cy.log("Current baseUrl" + Cypress.config('baseUrl'))
+    cy.log("Current baseUrl" + Cypress.config('baseUrl'))
   });
   it("Navbar are displayed", () => {
     cy.get("[data-cy='navbar']").should("be.visible");
@@ -19,26 +19,36 @@ describe("Verifier App", () => {
     cy.get(":nth-child(1) > .d-flex").should("be.visible");
   });
   it("Navbar are displayed the language", () => {
-    cy.get(":nth-child(2) > .d-flex").should("be.visible");
-    cy.get(":nth-child(2) > .d-flex").click(); // selector #header-tabs > li:nth-child(2) > a - href="#" (заглушка)
-    cy.get("[data-cy='accountMenu'] > .d-flex").should("have.length", 4); //ER = 4, AR = 1
+    cy.get(':nth-child(2) > .d-flex').should("be.visible");
+    cy.get("#header-tabs > li:nth-child(2) > a").click();
+    cy.get("#header-tabs > li.dropdown.show.nav-item > div > button:nth-child(1)").click();
+    cy.get(":nth-child(1) > .d-flex").contains("Home");
+    cy.get("#header-tabs > li:nth-child(2) > a").click();
+    cy.get("#header-tabs > li:nth-child(2) > div > button:nth-child(2)").click();
+    cy.get(":nth-child(1) > .d-flex").contains("Accueil");
+    cy.get("#header-tabs > li:nth-child(2) > a").click();
+    cy.get("#header-tabs > li:nth-child(2) > div > button:nth-child(3)").click();
+    cy.get(":nth-child(1) > .d-flex").contains("Главная");
+    cy.get("#header-tabs > li:nth-child(2) > a").click();
+    cy.get("#header-tabs > li:nth-child(2) > div > button:nth-child(4)").click();
+    cy.get(":nth-child(1) > .d-flex").contains("Головна");
   });
   it("Navbar are displayed the account", () => {
     cy.get("[data-cy='accountMenu'] > .d-flex").should("be.visible");
-    cy.get("[data-cy='accountMenu'] > .d-flex").click(); // href="#" (заглушка)
-    cy.get("[data-cy='accountMenu'] > .d-flex").should("have.length", 2); //ER = 2, AR = 1
-    //sign in
-    cy.get("[data-cy='login']").find("[a#login-item.dropdown-item]").click();
-    cy.url("https://sqlverifier-live-6e21ca0ed768.herokuapp.com/login").should(
-      "include",
-      "/login"
-    ); // ER = /login, AR = /account/register
-    //register
-    cy.get("[data-cy='register']")
-      .find("#account-menu > div > a:nth-child(2)")
-      .click();
-    cy.url(
-      "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/account/register"
-    ).should("include", "/account/register");
+
+    //sign in by user
+    cy.get("[data-cy='accountMenu'] > .d-flex").click(); 
+    cy.get("[data-cy='login']").click();
+    cy.get("[data-cy='username']").type("ksu158");
+    cy.get("[data-cy='password']").type("12345");
+    cy.get("[data-cy='submit']").click();
+    cy.get('.btn-secondary').click();
+
+    //sing in by admin
+    cy.get("[data-cy='accountMenu'] > .d-flex").click(); 
+    cy.get("[data-cy='login']").click();
+    cy.get("[data-cy='username']").type("admin_automation");
+    cy.get("[data-cy='password']").type("admin_automation");
+    cy.get("[data-cy='submit']").click();
   });
 });
