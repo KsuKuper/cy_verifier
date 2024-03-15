e95285d5bc63fcd4d648cbc3c365a19421593d8c
KsuKuper <nekrashevich.k@mail.ru>
2024-03-15 09:51:33 +0300

    updated hw_5.2.4

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: "33ivdp",   
+    projectId: "33ivdp",
     "baseUrl": "https://sqlverifier-staging-08050d656f7a.herokuapp.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
